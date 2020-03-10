e4b17c1f5903f85dd77d5d645f55c66cdc7869c4
jedrzejpisarski <60627936+jedrzejpisarski@users.noreply.github.com>
2020-03-10 19:00:53 +0100

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -59,12 +59,19 @@
       thisProduct.id = id;
       thisProduct.data = data;
 
-      thisProduct. renderInMenu();
-
+      thisProduct.renderInMenu();
       console.log('new Product:', thisProduct);
 
-      thisProduct. initAccordion();
+      thisProduct.getElements()
+      console.log('new Product:', thisProduct);
 
+      thisProduct.initAccordion();
+      console.log('new Product:', thisProduct);
+
+      thisProduct.initOrderForm();
+      console.log('new Product:', thisProduct);
+
+      thisProduct.processOrder();
       console.log('new Product:', thisProduct);
     }
     renderInMenu(){
@@ -84,7 +91,18 @@
       menuContainer.appendChild(thisProduct.element);
 
     }
-  initAccordion(){
+
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
+    initAccordion(){
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
@@ -117,6 +135,67 @@
       /* END: click event listener to trigger */
     }
   }
+
+  initOrderForm ();
+
+  const thisProduct = this;
+  console.log('initOrderForm');
+
+  thisProduct.form.addEventListener('submit', function(event){
+    event.preventDefault();
+    thisProduct.processOrder();
+  });
+  
+  for(let input of thisProduct.formInputs){
+    input.addEventListener('change', function(){
+      thisProduct.processOrder();
+    });
+  }
+  
+  thisProduct.cartButton.addEventListener('click', function(event){
+    event.preventDefault();
+    thisProduct.processOrder();
+  });
+
+  processOrder();
+
+  const thisProduct = this;
+  console.log('processOrder');
+  
+  /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+  const formData = utilis.serializeFormToObject(thisProduct.form);
+  console.log('formData', formData);
+  
+  /* set variable price to equal thisProduct.data.price */
+
+  /* START LOOP: for each paramId in thisProduct.data.params */
+  for(let paramId of thisProduct.data.params) {  
+    /* save the element in thisProduct.data.params with key paramId as const param */
+    const param = document.thisProduct.data.params.getElementsByTagName('paramId');
+    /* START LOOP: for each optionId in param.options */
+    for(let optionId of param.option){
+      /* save the element in param.options with key optionId as const option */
+      const option = document.param.options.getElementByTagName('optionId')
+      /* START IF: if option is selected and option is not default */
+      const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+      
+      if(optionSelected && !option.default){
+        /* add price of option to variable price */
+        
+      /* END IF: if option is selected and option is not default */
+      }
+      /* START ELSE IF: if option is not selected and option is default */
+      else if (!optionSelected && option.default){
+      /* deduct price of option from price */
+      
+      /* END ELSE IF: if option is not selected and option is default */
+      }
+    /* END LOOP: for each optionId in param.options */
+    }
+  /* END LOOP: for each paramId in thisProduct.data.params */
+  }
+  /* set the contents of thisProduct.priceElem to be the value of variable price */
+}
 
   initData: function(){
     const thisApp = this;
