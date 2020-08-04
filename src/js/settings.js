71c86c55bce8ebc2bf4a0caec35ddd08065f670d
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-08-04 23:11:52 +0200

    change

--- src/js/settings.js
+++ src/js/settings.js
@@ -69,7 +69,7 @@ export const select = {
     address: '[name="address"]',
   },
   nav: {
-    links: '.main-nav a',
+    links: '.main-nav a, .buttons a',
   },
 };
 
@@ -113,7 +113,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
